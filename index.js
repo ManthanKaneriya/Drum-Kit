881e7b14a7de6d4d1d11486baa48c65c99d55f8d
Manthan Kaneriya <kaneriyamanthan@gmail.com>
2024-05-15 23:50:37 +0530

    Add Keypress Event Listener

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.querySelector(".w").addEventListener("click", function () {
+/* document.querySelector(".w").addEventListener("click", function () {
     var audio = new Audio("./sounds/tom-1.mp3");
     audio.play();
 });
@@ -25,5 +25,51 @@ document.querySelector(".k").addEventListener("click", function () {
 document.querySelector(".l").addEventListener("click", function () {
     var audio = new Audio("./sounds/kick-bass.mp3");
     audio.play();
+}); */
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for(i=0; i<numberOfDrumButtons; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        makeSound(this.innerHTML);
+    });
+}
+
+// Detecting Keyboard Press
+document.addEventListener("keypress", function(event){
+    console.log(event.key);
+    makeSound(event.key);
 });
 
+function makeSound(key){
+    console.log(key);
+    switch (key) {
+        case "w":
+            var audio = new Audio("./sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("./sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("./sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("./sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("./sounds/snare.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("./sounds/crash.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("./sounds/kick-bass.mp3");
+            audio.play();
+            break;
+}}
